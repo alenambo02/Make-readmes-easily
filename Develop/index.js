0ce4058cfef05a2223c8cebeaf73d2742f86da84
Ale i <allleizq@gmail.com>
2022-07-15 22:35:05 -0700

    require needed items, created some prompts

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,38 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
+
+inquirer 
+    .prompt([
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Give me a description of your project?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What are the installation instructions for the project?',
+        name: 'instructions',
+    },
+  
+    ])
+ 
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
 function init() {}
